1354029593249aebe2f56b085fb6238c4c525245
Samo Lisý <g.vvoody@gmail.com>
2020-10-01 15:44:00 +0200

    nette 3 compatibility

--- src/Route.php
+++ src/Route.php
@@ -2,9 +2,8 @@
 
 namespace ADT;
 
-use Nette\Application\Request as AppRequest;
 use Nette\Http\Url;
-
+use Nette\Http\UrlScript;
 
 /**
  * The bidirectional route with non-standard port support for absolute masks.
@@ -15,9 +14,9 @@ class Route extends \Nette\Application\Routers\Route
 	 * Constructs absolute URL from Request object.
 	 * @return string|null
 	 */
-	public function constructUrl(AppRequest $appRequest, Url $refUrl)
+	public function constructUrl(array $params, UrlScript $refUrl): ?string
 	{
-		$url = parent::constructUrl($appRequest, $refUrl);
+		$url = parent::constructUrl($params, $refUrl);
 
 		if ($url !== null && ! in_array($refUrl->getPort(), Url::$defaultPorts, true)) {
 			$nurl = new Url($url);
